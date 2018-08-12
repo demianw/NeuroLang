28f9534aae5f58d9581018e51baea307e4adf9d5
Demian Wassermann <demian.wassermann@inria.fr>
2018-08-12 14:57:40 +0200

    Added a walker to extract the restrictions for each free variable in Safe-range normal form

--- neurolang/datalog_walkers.py
+++ neurolang/datalog_walkers.py
@@ -1,25 +1,14 @@
 from operator import and_, or_, invert
-import typing
-import logging
-
 
 from . import expression_walker as ew
-from . import expressions as exp
 from .neurolang import NeuroLangException
 
-
-logger = logging.getLogger()
 
 __all__ = [
     'undefined',
     'SafeRangeVariablesWalker',
     'NeuroLangException'
 ]
-
-
-F_ = exp.FunctionApplication
-C_ = exp.Constant
-S_ = exp.Symbol
 
 
 class Undefined:
@@ -50,25 +39,8 @@ class Undefined:
     def __ror__(self, other):
         return self
 
-    def __invert__(self):
-        return self
-
 
 undefined = Undefined()
-
-
-def atleast_one_argument_is_application(expression):
-    return any(
-        isinstance(a, F_)
-        for a in expression.args
-    )
-
-
-def no_argument_is_application(expression):
-    return all(
-        not isinstance(a, F_)
-        for a in expression.args
-    )
 
 
 def application_is_not_logical_op(expression):
@@ -82,14 +54,7 @@ class Intersection(frozenset):
     Set of restrictions to be intersected
     to obtain the restricted domain.
     '''
-    def __or__(self, other):
-        if isinstance(other, Intersection):
-            return Intersection(super().__or__(other))
-        else:
-            raise ValueError('parameter must be Intersection')
-
-    def __ror__(self, other):
-        return self.__or__(other)
+    pass
 
 
 class Union(frozenset):
@@ -97,14 +62,7 @@ class Union(frozenset):
     Set of restrictions to be merged
     to obtain the restricted domain.
     '''
-    def __or__(self, other):
-        if isinstance(other, Union):
-            return Union(super().__or__(other))
-        else:
-            raise ValueError('parameter must be Union')
-
-    def __ror__(self, other):
-        return self.__or__(other)
+    pass
 
 
 class SafeRangeVariablesWalker(ew.PatternWalker):
@@ -115,69 +73,53 @@ class SafeRangeVariablesWalker(ew.PatternWalker):
     For each variable it also obtains a set of intersection or
     union of predicates to be used to compute the range of each
     variable.
+    the domain.
     '''
 
-    @ew.add_match(F_[bool](C_(and_), ...))
+    @ew.add_match(ew.FunctionApplication[bool](ew.Constant(and_), ...))
     def conjunction(self, expression):
         restrictors = dict()
         for arg in expression.args:
             arg_rest = self.walk(arg)
-            if arg_rest is undefined:
-                return undefined
             for k, v in arg_rest.items():
-                restrictor = v | restrictors.get(k, Intersection())
-                restrictors[k] = restrictor
+                restrictors[k] = v | restrictors.get(k, Intersection())
 
         return restrictors
 
-    @ew.add_match(F_[bool](C_(or_), ...))
+    @ew.add_match(ew.FunctionApplication[bool](ew.Constant(or_), ...))
     def disjunction(self, expression):
-        args = expression.args
-        restrictors = self.walk(args[0])
-        if restrictors is undefined:
-            return undefined
-        for arg in args:
+        restrictors = None
+        for arg in expression.args:
             arg_rest = self.walk(arg)
-            if arg_rest is undefined:
-                return undefined
-            for k in list(restrictors.keys()):
-                if k not in arg_rest:
-                    del restrictors[k]
-                else:
-                    r1 = restrictors[k]
-                    r2 = arg_rest[k]
-
-                    if not isinstance(r1, Union):
-                        if len(r1) == 1:
-                            r1 = Union(r1)
-                        else:
-                            r1 = Union({r1})
-
-                    if not isinstance(r2, Union):
-                        if len(r2) == 1:
-                            r2 = Union(r2)
-                        else:
-                            r2 = Union({r2})
-                    restrictors[k] = r1 | r2
+            if restrictors is None:
+                restrictors = arg_rest.copy()
+            else:
+                for k in set(restrictors.keys()):
+                    if k not in arg_rest:
+                        del restrictors[k]
+                    else:
+                        restrictors[k] = {
+                            Union((
+                                Intersection(arg_rest[k]),
+                                Intersection(restrictors[k])
+                            ))
+                        }
 
         return restrictors
 
     @ew.add_match(
-        F_[bool](
-            C_(invert),
-            (exp.Expression[bool],)
+        ew.FunctionApplication[bool](
+            ew.Constant(invert),
+            (ew.FunctionApplication[bool](ew.Constant, ...),)
         ),
-        lambda e: (
-            not isinstance(e.args[0], F_[bool]) or
-            no_argument_is_application(e.args[0])
-        )
+        lambda expression: application_is_not_logical_op(expression.args[0])
     )
     def inversion(self, expression):
         return dict()
 
     @ew.add_match(
-        F_[bool](C_, ...),
-        no_argument_is_application
+        ew.FunctionApplication[bool](ew.Constant, ...),
+        application_is_not_logical_op
     )
     def fa(self, expression):
         restrictors = {
@@ -186,7 +128,7 @@ class SafeRangeVariablesWalker(ew.PatternWalker):
         }
         return restrictors
 
-    @ew.add_match(exp.ExistentialPredicate[bool])
+    @ew.add_match(ew.ExistentialPredicate[bool])
     def ex(self, expression):
         restrictors = self.walk(expression.body)
         if expression.head._symbols.issubset(restrictors.keys()):
@@ -200,212 +142,3 @@ class SafeRangeVariablesWalker(ew.PatternWalker):
     @ew.add_match(...)
     def _(self, expression):
         raise NeuroLangException('Expression not in safe-range normal form')
-
-
-class VariableSubstitutionWalker(ew.PatternWalker):
-    def __init__(self):
-        self.seen_variables = set()
-
-    @ew.add_match(
-        F_[bool](C_, ...),
-        lambda e: e.functor.value in (and_, or_, invert)
-    )
-    def logical_application(self, expression):
-        new_args = []
-        changed = False
-        for arg in expression.args:
-            new_arg = self.walk(arg)
-            new_args.append(new_arg)
-            changed |= new_arg is not arg
-
-        if changed:
-            return F_[bool](
-                expression.functor, tuple(new_args)
-            )
-        else:
-            return expression
-
-    @ew.add_match(
-        F_[bool](C_, ...),
-        no_argument_is_application
-    )
-    def fa(self, expression):
-        self.seen_variables |= expression._symbols
-        return expression
-
-    @ew.add_match(exp.Quantifier[bool])
-    def quantifier(self, expression):
-        replacement_symbols = dict()
-        for s in expression.head._symbols:
-            new_s = s
-            while new_s in self.seen_variables:
-                new_s = S_[new_s.type](new_s.name + '_')
-            if new_s is not s:
-                replacement_symbols[s] = new_s
-
-        if len(replacement_symbols) > 0:
-            rsw = ew.ReplaceSymbolWalker(replacement_symbols)
-            expression = type(expression)(
-                rsw.walk(expression.head),
-                rsw.walk(expression.body)
-            )
-
-        self.seen_variables |= expression.body._symbols
-        self.seen_variables |= expression.head._symbols
-
-        return expression
-
-
-class ConvertToSNRFWalker(ew.ExpressionWalker):
-    @ew.add_match(F_[bool](
-        C_(invert),
-        (F_[bool](C_(invert), ...),)
-    ))
-    def push_neg_double_neg(self, expression):
-        return self.walk(expression.args[0].args[0])
-
-    @ew.add_match(
-        F_[bool](
-            C_(invert),
-            (F_[bool](C_(and_), ...),)
-        )
-    )
-    def push_neg_and(self, expression):
-        args = expression.args[0].args
-        return self.walk(F_[bool](
-            C_[typing.Callable[[bool] * len(args), bool]](or_),
-            tuple(
-                F_[bool](
-                    C_[typing.Callable[[bool], bool]](invert),
-                    (a,)
-                )
-                for a in args
-            )
-        ))
-
-    @ew.add_match(
-        F_[bool](
-            C_(invert),
-            (F_[bool](C_(or_), ...),)
-        )
-    )
-    def push_neg_or(self, expression):
-        args = expression.args[0].args
-        return self.walk(F_[bool](
-            C_[typing.Callable[[bool] * len(args), bool]](and_),
-            tuple(
-                F_[bool](
-                    C_[typing.Callable[[bool], bool]](invert),
-                    (a,)
-                )
-                for a in args
-            )
-        ))
-
-    @ew.add_match(exp.UniversalPredicate[bool])
-    def universal_to_existential(self, expression):
-        return self.walk(
-            F_[bool](
-                C_[typing.Callable[[bool], bool]](invert),
-                (exp.ExistentialPredicate[bool](
-                    expression.head,
-                    F_[bool](
-                        C_[typing.Callable[[bool], bool]](invert),
-                        (expression.body,)
-                    )
-                ),)
-            )
-        )
-
-
-class FlattenMultipleLogicalOperators(ew.ExpressionWalker):
-    @ew.add_match(
-        F_[bool](C_(and_), ...),
-        lambda e: any(
-            isinstance(a, exp.FunctionApplication[bool]) and
-            a.functor == C_(and_)
-            for a in e.args
-        )
-    )
-    def flatten_and(self, expression):
-        new_args = []
-        for a in expression.args:
-            if (
-                isinstance(a, exp.FunctionApplication[bool]) and
-                a.functor == C_(and_)
-            ):
-                new_args.extend(a.args)
-            else:
-                new_args.append(a)
-
-        functor = C_[typing.Callable[[bool] * len(new_args), bool]](and_)
-        return self.walk(F_[bool](functor, tuple(new_args)))
-
-    @ew.add_match(
-        F_[bool](C_(or_), ...),
-        lambda e: any(
-            isinstance(a, exp.FunctionApplication[bool]) and
-            a.functor == C_(or_)
-            for a in e.args
-        )
-    )
-    def flatten_or(self, expression):
-        new_args = []
-        for a in expression.args:
-            if (
-                isinstance(a, exp.FunctionApplication[bool]) and
-                a.functor == C_(or_)
-            ):
-                new_args.extend(a.args)
-            else:
-                new_args.append(a)
-        functor = C_[typing.Callable[[bool] * len(new_args), bool]](or_)
-        return self.walk(F_[bool](functor, tuple(new_args)))
-
-
-def expression_to_SRNF_and_range(expression):
-    '''
-    Converts a first-order logic expression to SRNF
-    and produces the range
-    '''
-
-    for _, e in ew.expression_iterator(expression):
-        if isinstance(e, F_[bool]):
-            if (
-                e.functor.value in (and_, or_, invert) and
-                all(e.type is bool for e in e.args)
-            ):
-                pass
-            elif all(not isinstance(a, exp.Definition) for a in e.args):
-                pass
-            else:
-                raise ValueError(
-                    f'{expression} is not a valid datalog expression'
-                )
-        elif isinstance(e, (
-            exp.ExistentialPredicate[bool], exp.UniversalPredicate[bool],
-            S_, C_
-        )):
-            pass
-        else:
-                raise ValueError(
-                    f'{expression} is not a valid datalog expression'
-                )
-
-        logger.info('Variable substitution')
-        vsw = VariableSubstitutionWalker()
-        expression = vsw.walk(expression)
-
-        logger.info('Flatten logical ops')
-        fw = FlattenMultipleLogicalOperators()
-        expression = fw.walk(expression)
-
-        logger.info('Convert to SNRF')
-        csnrf = ConvertToSNRFWalker()
-        expression = csnrf.walk(expression)
-
-        logger.info('Compute range')
-        srvw = SafeRangeVariablesWalker()
-        var_range = srvw.walk(expression)
-
-        return expression, var_range
--- neurolang/tests/test_datalog_walkers.py
+++ neurolang/tests/test_datalog_walkers.py
@@ -1,8 +1,5 @@
 import pytest
 
-from operator import or_, and_, invert
-import typing
-
 from .. import datalog_walkers as dw
 from ..neurolang import (
     Constant, Symbol, FunctionApplication,
@@ -14,24 +11,6 @@ S_ = Symbol
 F_ = FunctionApplication
 E_ = ExistentialPredicate
 A_ = UniversalPredicate
-
-and_binary_functor = C_[typing.Callable[[bool, bool], bool]](and_)
-or_binary_functor = C_[typing.Callable[[bool, bool], bool]](or_)
-invert_functor = C_[typing.Callable[[bool], bool]](invert)
-
-
-def test_undefined():
-    a = set()
-
-    assert (a + dw.undefined) is dw.undefined
-    assert (dw.undefined + a) is dw.undefined
-    assert (a - dw.undefined) is dw.undefined
-    assert (dw.undefined - a) is dw.undefined
-    assert (a | dw.undefined) is dw.undefined
-    assert (dw.undefined | a) is dw.undefined
-    assert (a & dw.undefined) is dw.undefined
-    assert (dw.undefined & a) is dw.undefined
-    assert (~dw.undefined) is dw.undefined
 
 
 def test_atom():
@@ -72,16 +51,6 @@ def test_conjunction():
         c: dw.Intersection({f})
     }
 
-    e = f & E_[bool](S_('x'), g)
-    restrictors = srv.walk(e)
-
-    assert restrictors is dw.undefined
-
-    e = E_[bool](S_('x'), g) & f
-    restrictors = srv.walk(e)
-
-    assert restrictors is dw.undefined
-
 
 def test_disjunction():
     srv = dw.SafeRangeVariablesWalker()
@@ -89,41 +58,15 @@ def test_disjunction():
     a = C_(sum)
     b = S_('b')
     c = S_('c')
-    d = C_(any)
-    e = C_(all)
+    d = C_(lambda x: x % 2 == 0)
 
     f = F_[bool](a, (b, c, C_(1)))
     g = F_[bool](d, (b,))
-
-    exp = f | g
-    restrictors = srv.walk(exp)
-    res = {b: dw.Union((f, g))}
-
-    assert restrictors == res
-
-    h = F_[bool](e, (b,))
-    exp = (f & g) | h
-    restrictors = srv.walk(exp)
-    res = {b: dw.Union((dw.Intersection((f, g)), h))}
-
-    assert restrictors == res
-
-    h = F_[bool](e, (b,))
-    exp = f | (g & h)
-    restrictors = srv.walk(exp)
-    res = {b: dw.Union((dw.Intersection((h, g)), f))}
-
-    assert restrictors == res
-
-    e = f | E_[bool](S_('x'), g)
+    e = f | g
     restrictors = srv.walk(e)
-
-    assert restrictors is dw.undefined
-
-    e = E_[bool](S_('x'), g) | f
-    restrictors = srv.walk(e)
-
-    assert restrictors is dw.undefined
+    assert restrictors == {b: {
+        dw.Union((dw.Intersection({f}), dw.Intersection({g})))
+    }}
 
 
 def test_inversion():
@@ -177,138 +120,3 @@ def test_not_srnf():
 
     with pytest.raises(dw.NeuroLangException):
         srv.walk(e2)
-
-
-def test_replace_variables():
-    vsw = dw.VariableSubstitutionWalker()
-
-    a = C_(sum)
-    b = S_('b')
-    b_ = S_('b_')
-    c = S_('c')
-
-    f = F_[bool](a, (b, c, C_(1)))
-    e = E_[bool](b, f)
-
-    exp = f & e
-    exp_new = vsw.walk(exp)
-    expected_result = (f & E_[bool](b_, F_(a, (b_, c, C_(1))))).cast(bool)
-
-    assert exp_new == expected_result
-
-    vsw = dw.VariableSubstitutionWalker()
-
-    exp_new_2 = vsw.walk(exp_new)
-
-    assert exp_new is exp_new_2
-
-
-def test_push_neg_double_neg():
-    csnrf = dw.ConvertToSNRFWalker()
-
-    a = S_('a')
-    e = (~(~a).cast(bool)).cast(bool)
-
-    res = csnrf.walk(e)
-
-    assert res == a
-
-
-def test_push_neg_and():
-    csnrf = dw.ConvertToSNRFWalker()
-
-    a = S_[bool]('a')
-    b = S_[bool]('b')
-    e = (~(a & b).cast(bool)).cast(bool)
-
-    res = csnrf.walk(e)
-    assert res == or_binary_functor(invert_functor(a), invert_functor(b))
-
-
-def test_push_neg_or():
-    csnrf = dw.ConvertToSNRFWalker()
-
-    a = S_[bool]('a')
-    b = S_[bool]('b')
-    e = (~(a | b).cast(bool)).cast(bool)
-
-    res = csnrf.walk(e)
-    assert res == and_binary_functor(invert_functor(a), invert_functor(b))
-
-
-def test_univ_to_ex():
-    csnrf = dw.ConvertToSNRFWalker()
-
-    a = S_[typing.Callable[[bool], bool]]('a')
-    b = S_[bool]('b')
-    e = A_[bool](b, a(b))
-
-    exp_res = invert_functor(E_[bool](b, invert_functor(a(b))))
-
-    res = csnrf.walk(e)
-
-    assert res == exp_res
-
-
-def test_flatten_and():
-    fw = dw.FlattenMultipleLogicalOperators()
-
-    args = [S_[bool](f'a{i}') for i in range(5)]
-
-    exp = and_binary_functor(
-        and_binary_functor(
-            args[0],
-            and_binary_functor(
-                args[1],
-                and_binary_functor(args[2], args[3])
-            ),
-        ),
-        args[4]
-    )
-
-    res = fw.walk(exp)
-
-    assert res.functor == C_(and_)
-    assert set(res.args) == set(args)
-
-
-def test_flatten_or():
-    fw = dw.FlattenMultipleLogicalOperators()
-
-    args = [S_[bool](f'a{i}') for i in range(5)]
-
-    exp = or_binary_functor(
-        or_binary_functor(
-            args[0],
-            or_binary_functor(
-                args[1],
-                or_binary_functor(args[2], args[3])
-            ),
-        ),
-        args[4]
-    )
-
-    res = fw.walk(exp)
-
-    assert res.functor == C_(or_)
-    assert set(res.args) == set(args)
-
-
-def test_SNRF_and_range():
-    R = C_[typing.Callable[[bool], bool]](any)
-    Q = C_[typing.Callable[[bool], bool]](all)
-
-    x = S_[bool]('x')
-    y = S_[bool]('y')
-
-    exp = R(x)
-
-    res, r = dw.expression_to_SRNF_and_range(exp)
-    assert r == {x: dw.Intersection((exp,))}
-    assert res is exp
-
-    exp = A_[bool](x, invert_functor(Q(x, y)))
-    res, r = dw.expression_to_SRNF_and_range(exp)
-    exp_res = invert_functor(E_[bool](x, Q(x, y)))
-    assert r == {}
-    assert res == exp_res
